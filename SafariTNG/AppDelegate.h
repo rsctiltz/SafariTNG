71b5b01dea1694059d40b347ca4bb5f73cbd6557
rsctiltz <rsctiltz@gmail.com>
2014-05-14 10:37:18 -0500

    Initial Commit

---
+++ SafariTNG/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SafariTNG
+//
+//  Created by Ryan Tiltz on 5/14/14.
+//  Copyright (c) 2014 Ryan Tiltz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
